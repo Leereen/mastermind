b1630a548c5149196c0b9216f1e6bd1ca6fff59c
Jessica <synowski.jessica@hotmail.fr>
2024-05-31 17:58:44 +0200

    fct drag and drop ok pour  couleur bleu sur  élément boule vierge

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -1,36 +1,43 @@
-const selectBoules = document.querySelectorAll(".selectBoule");
+const selectBoule = document.querySelector(".selectBoule");
 let selectedColor;
 const dropBoule = document.querySelector(".bouleVierge");
 console.log(selectBoule);
-for (let selectBoule of selectBoules) {
-  selectBoule.ondragstart = (e) => {
-    e.dataTransfer.setData("text/plain", selectBoule.innerHTML);
-    selectBoule.classList.add("dragged");
-    selectedColor = e.dataset.color;
-  };
+console.log(dropBoule);
+//for (let selectBoule of selectBoules) {
+selectBoule.ondragstart = (e) => {
+  e.dataTransfer.setData("text/plain", selectBoule.innerHTML);
+  selectedColor = selectBoule.dataset.color;
+  console.log(selectedColor);
+  //selectBoule.classList.add("dragged");
+};
 
-  dropBoule.ondragover = (e) => {
-    e.preventDefault();
-  };
+dropBoule.ondragover = (e) => {
+  e.preventDefault();
+};
 
-  // quand on survole la zone de drop
-  dropBoule.ondragenter = () => {
-    dropBoule.classList.add("dropHover");
-  };
+// quand on survole la zone de drop
+dropBoule.ondragenter = () => {
+  //dropBoule.classList.add("dropHover");
+  dropBoule.dataset.color = selectedColor;
+  console.log(dropBoule.dataset.color);
+};
 
-  // quand on quitte la zone de drop
-  dropBoule.ondragleave = () => {
-    dropBoule.classList.remove("dropHover");
-  };
+// quand on quitte la zone de drop
+dropBoule.ondragleave = () => {
+  //dropBoule.classList.remove("dropHover");
+};
 
-  //remet à 0 le css sur l'élément glisser
-  selectBoule.ondragend = () => {
-    selectBoule.classList.remove("dragged");
-  };
+//remet à 0 le css sur l'élément glisser
+selectBoule.ondragend = () => {
+  //selectBoule.classList.remove("dragged");
+};
 
-  // permet de changer la valeur de la boule drop avec la nouvelle
-  selectBoule.ondrop = (e) => {
-    dropBoule.innerHTML = e.dataTransfer.getData("text/plain");
-    dropBoule.classList.remove("dropHover");
-  };
-}
+// permet de changer la valeur de la boule drop avec la nouvelle
+selectBoule.ondrop = (e) => {
+  dropBoule.innerHTML = e.dataTransfer.getData("text/plain");
+  dropBoule.dataset.color = selectedColor;
+  console.log(dropBoule.dataset.color);
+
+  //dropBoule.classList.remove("dropHover");
+};
+//}
